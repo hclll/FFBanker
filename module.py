64b585be4af7041701a52be6c2a81daadf93ebcc
hclll <syuanchen0000@gmail.com>
2025-03-03 19:56:39 -0600

    add module and parser

---
+++ module.py
@@ -0,0 +1,113 @@
+class Die:
+    def __init__(self, lower_left_x, lower_left_y, upper_right_x, upper_right_y):
+        self.lower_left_x = lower_left_x
+        self.lower_left_y = lower_left_y
+        self.upper_right_x = upper_right_x
+        self.upper_right_y = upper_right_y
+        self.inputs = {}
+        self.outputs = {}
+
+    def add_input(self, name, x, y):
+        self.inputs[name] = (x, y)
+
+    def add_output(self, name, x, y):
+        self.outputs[name] = (x, y)
+
+
+class FlipFlop:
+    def __init__(self, bits, name, width, height, pin_count):
+        self.bits = bits
+        self.name = name
+        self.width = width
+        self.height = height
+        self.pin_count = pin_count
+        self.pins = {}
+
+    def add_pin(self, pin_name, x, y):
+        self.pins[pin_name] = (x, y)
+
+
+class Gate:
+    def __init__(self, name, width, height, pin_count):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.pin_count = pin_count
+        self.pins = {}
+
+    def add_pin(self, pin_name, x, y):
+        self.pins[pin_name] = (x, y)
+
+
+class CellLibrary:
+    def __init__(self):
+        self.flip_flops = {}
+        self.gates = {}
+
+    def add_flip_flop(self, flip_flop):
+        self.flip_flops[flip_flop.name] = flip_flop
+
+    def add_gate(self, gate):
+        self.gates[gate.name] = gate
+
+
+class Instance:
+    def __init__(self, name, cell_type, x, y):
+        self.name = name
+        self.cell_type = cell_type
+        self.x = x
+        self.y = y
+
+
+class Net:
+    def __init__(self, name):
+        self.name = name
+        self.pins = []
+
+    def add_pin(self, pin):
+        self.pins.append(pin)
+
+
+class Netlist:
+    def __init__(self):
+        self.nets = {}
+
+    def add_net(self, net):
+        self.nets[net.name] = net
+
+
+class Bin:
+    def __init__(self, width, height, max_utilization):
+        self.width = width
+        self.height = height
+        self.max_utilization = max_utilization
+
+
+class PlacementRow:
+    def __init__(self, start_x, start_y, site_width, site_height, total_sites):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.site_width = site_width
+        self.site_height = site_height
+        self.total_sites = total_sites
+
+
+class TimingInfo:
+    def __init__(self, displacement_delay):
+        self.displacement_delay = displacement_delay
+        self.qpin_delays = {}
+        self.timing_slacks = {}
+
+    def add_qpin_delay(self, cell_name, delay):
+        self.qpin_delays[cell_name] = delay
+
+    def add_timing_slack(self, instance_name, pin_name, slack):
+        self.timing_slacks[(instance_name, pin_name)] = slack
+
+
+class PowerInfo:
+    def __init__(self):
+        self.gate_powers = {}
+
+    def add_power(self, cell_name, power):
+        self.gate_powers[cell_name] = power
---
+++ parser.py
@@ -0,0 +1,116 @@
+import re
+from module import *
+
+class Parser:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.die = None
+        self.cell_library = CellLibrary()
+        self.instances = []
+        self.netlist = Netlist()
+        self.bin_constraints = None
+        self.placement_rows = []
+        self.timing_info = None
+        self.power_info = PowerInfo()
+
+    def parse(self):
+        with open(self.file_path, "r") as file:
+            lines = file.readlines()
+
+        i = 0
+        while i < len(lines):
+            tokens = lines[i].split()
+            if not tokens:
+                i += 1
+                continue
+
+            keyword = tokens[0]
+
+            if keyword == "Alpha":
+                alpha = float(tokens[1])
+            elif keyword == "Beta":
+                beta = float(tokens[1])
+            elif keyword == "Gamma":
+                gamma = float(tokens[1])
+            elif keyword == "Lambda":
+                lambda_ = float(tokens[1])
+            elif keyword == "DieSize":
+                self.die = Die(*map(int, tokens[1:]))
+            elif keyword == "NumInput":
+                num_inputs = int(tokens[1])
+                for _ in range(num_inputs):
+                    i += 1
+                    input_tokens = lines[i].split()
+                    self.die.add_input(input_tokens[1], int(input_tokens[2]), int(input_tokens[3]))
+            elif keyword == "NumOutput":
+                num_outputs = int(tokens[1])
+                for _ in range(num_outputs):
+                    i += 1
+                    output_tokens = lines[i].split()
+                    self.die.add_output(output_tokens[1], int(output_tokens[2]), int(output_tokens[3]))
+            elif keyword == "FlipFlop":
+                flip_flop = FlipFlop(int(tokens[1]), tokens[2], int(tokens[3]), int(tokens[4]), int(tokens[5]))
+                for _ in range(flip_flop.pin_count):
+                    i += 1
+                    pin_tokens = lines[i].split()
+                    flip_flop.add_pin(pin_tokens[1], int(pin_tokens[2]), int(pin_tokens[3]))
+                self.cell_library.add_flip_flop(flip_flop)
+            elif keyword == "Gate":
+                gate = Gate(tokens[1], int(tokens[2]), int(tokens[3]), int(tokens[4]))
+                for _ in range(gate.pin_count):
+                    i += 1
+                    pin_tokens = lines[i].split()
+                    gate.add_pin(pin_tokens[1], int(pin_tokens[2]), int(pin_tokens[3]))
+                self.cell_library.add_gate(gate)
+            elif keyword == "NumInstances":
+                num_instances = int(tokens[1])
+                for _ in range(num_instances):
+                    i += 1
+                    inst_tokens = lines[i].split()
+                    self.instances.append(Instance(inst_tokens[1], inst_tokens[2], int(inst_tokens[3]), int(inst_tokens[4])))
+            elif keyword == "NumNets":
+                num_nets = int(tokens[1])
+                for _ in range(num_nets):
+                    i += 1
+                    net_tokens = lines[i].split()
+                    net = Net(net_tokens[1])
+                    num_pins = int(net_tokens[2])
+                    for _ in range(num_pins):
+                        i += 1
+                        pin_tokens = lines[i].split()
+                        net.add_pin(pin_tokens[1])
+                    self.netlist.add_net(net)
+            elif keyword == "BinWidth":
+                bin_width = int(tokens[1])
+            elif keyword == "BinHeight":
+                bin_height = int(tokens[1])
+            elif keyword == "BinMaxUtil":
+                self.bin_constraints = Bin(bin_width, bin_height, float(tokens[1]))
+            elif keyword == "PlacementRows":
+                self.placement_rows.append(PlacementRow(*map(int, tokens[1:])))
+            elif keyword == "DisplacementDelay":
+                self.timing_info = TimingInfo(float(tokens[1]))
+            elif keyword == "QpinDelay":
+                self.timing_info.add_qpin_delay(tokens[1], float(tokens[2]))
+            elif keyword == "TimingSlack":
+                self.timing_info.add_timing_slack(tokens[1], tokens[2], float(tokens[3]))
+            elif keyword == "GatePower":
+                self.power_info.add_power(tokens[1], float(tokens[2]))
+
+            i += 1
+
+        return self
+
+
+if __name__ == "__main__":
+    parser = Parser("bm/sampleCase")
+    parsed_data = parser.parse()
+
+    print("Die Size:", vars(parsed_data.die))
+    print("Number of Flip-Flops:", len(parsed_data.cell_library.flip_flops))
+    print("Number of Gates:", len(parsed_data.cell_library.gates))
+    print("Number of Instances:", len(parsed_data.instances))
+    print("Number of Nets:", len(parsed_data.netlist.nets))
+    print("Placement Rows:", len(parsed_data.placement_rows))
+    print("Timing Info:", vars(parsed_data.timing_info) if parsed_data.timing_info else "None")
+    print("Power Info:", vars(parsed_data.power_info))

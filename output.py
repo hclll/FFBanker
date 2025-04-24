bfa190bf6529ab1bd6d4d72e9d698e12d2f3a776
ramosa123 <unixanna@DESKTOP-EU623TK.>
2025-04-24 18:06:03 -0500

    Fixed output

--- output.py
+++ output.py
@@ -4,7 +4,7 @@ from preprocessing import annotate_ff_features, find_single_bit_ff, debanking_al
 import statistics
 import re
 
-def generate_output_file(parsed_data):
+def generate_output_file(parsed_data): 
 
     # Access cell library
     cell_lib = parsed_data.cell_library;
@@ -15,6 +15,7 @@ def generate_output_file(parsed_data):
 
     # Check that instances are FFs.
     for instance in all_instances:
+        #all_instances[instance].name = str("new_" + all_instances[instance].name);
         if all_instances[instance].cell_type in cell_lib.flip_flops:
             instances[instance] = all_instances[instance];
 
@@ -26,50 +27,38 @@ def generate_output_file(parsed_data):
 
         # Write instance name, cell name, and coordinates
         for key in instances:
-            file.write("Inst " + str(instances[key].name) + " " + str(instances[key].cell_type) + " "
+            file.write("Inst " + str("new_" + instances[key].name) + " " + str(instances[key].cell_type) + " "
                                + str(instances[key].x)+ " " + str(instances[key].y) + "\n");
 
         # Map old register pins to new register pins
         for key in instances:
 
-            # Notes:
-            # Check if the .pin_mapping dict is empty. If empty, it's a single-bit but NOT debanked register, so same pin names are used.
-            # Check if the pin mappings have to go in the order of D, Q, CLK. If so, I have to fix the empty dict case.
-            # Add case that checks for banked FFs and maps those. Differentiate multi-bit FF already present in input case vs after banking phase.
-
-            # Check if instance has not had its pins changed (currently, it only changes if it gets debanked in preprocessing)
-            if (not instances[key].pin_mapping):
+            # Check if instance has not had its pins changed (only possible if it is a single-bit FF that did not get banked)
+            if (not instances[key].pin_mapping): 
                 for pin in cell_lib.flip_flops[instances[key].cell_type].pins:
-                    file.write(str(instances[key].original_name) + "/" + str(pin) + " "
-                          + "map" + " " + str(instances[key].name) + "/" + str(pin) + "\n");
+                    file.write(str(instances[key].name) + "/" + str(pin) + " "
+                          + "map" + " " + str("new_" + instances[key].name) + "/" + str(pin) + "\n");
 
-            # If pin_mapping dict is not empty, this is a single-bit, debanked register. Debanked after preprocessing.
-            # New CLK currently is the same as the old CLK, as its pin should not change.
+            # If pin_mapping dict is not empty, this FF got debanked/banked at some point.
             else:
-                for pin in cell_lib.flip_flops[instances[key].cell_type].pins:
-                    if (pin == 'D'):
-                        for old_pin in instances[key].pin_mapping:
-                            if ("D" in str(old_pin)): # check that old_pin is a D-pin
-                                file.write(str(instances[key].original_name) + "/" + str(instances[key].pin_mapping[old_pin]) + " "
-                                + "map" + " " + str(instances[key].name) + "/" + str(pin) + "\n");
-                    elif (pin != 'D' and pin!= "Q"): # should be CLK
-                        file.write(str(instances[key].original_name) + "/" + str(pin) + " "
-                                + "map" + " " + str(instances[key].name) + "/" + str(pin) + "\n");
-                    elif (pin == 'Q'):
-                        for old_pin in instances[key].pin_mapping:
-                            if ("Q" in str(old_pin)): # check that old_pin is a Q-pin
-                                file.write(str(instances[key].original_name) + "/" + str(instances[key].pin_mapping[old_pin]) + " "
-                                + "map" + " " + str(instances[key].name) + "/" + str(pin) + "\n");
+                
+                # Check if this is a single-bit register (may not be necessary, so i set it to > 0)
+                if cell_lib.flip_flops[instances[key].cell_type].bits > 0:
+                    # Go through each pin and see what each maps to
+                    for pin in cell_lib.flip_flops[instances[key].cell_type].pins:
+                        for new_pin_key in instances[key].pin_mapping:
+                            if pin == new_pin_key:
+                                file.write(str(instances[key].pin_mapping[new_pin_key][0]) + "/" + str(instances[key].pin_mapping[new_pin_key][1]) + " " + "map" + " " 
+                                           + str("new_" + instances[key].name) + "/" + str(pin) + "\n");
+                    file.write(str(instances[key].pin_mapping[new_pin_key][0]) + "/" + str("CLK") + " " + "map" + " " + str(instances[key].name) + "/" + str("CLK") + "\n");
+                
 
 if __name__ == "__main__":
 
     # Parse input file and retrieve data
     parser = Parser("bm/sampleCase1")
     parsed_data = parser.parse()
-    
-    # Calculate median FF power, area, timing and label FFs based on medians
     cell_lib = parsed_data.cell_library
-    annotate_ff_features(cell_lib, parsed_data.timing_info, parsed_data.power_info)
 
     # Debank all multi-bit registers in the input file
     netlist = parsed_data.netlist
@@ -77,7 +66,7 @@ if __name__ == "__main__":
     
     # Generate output file mapping current design to the old design
     generate_output_file(parsed_data);
-        
+       
     
 
 

ff0608cda3b655c0bcf85382fe3ce931c2535a82
ramosa123 <unixanna@DESKTOP-EU623TK.>
2025-04-27 22:05:12 -0500

    Fixed debanking to not place off-site

--- debanking.py
+++ debanking.py
@@ -2,7 +2,7 @@
 from module import Die, CellLibrary, Instance, Netlist, Net, FlipFlop
 from parser import Parser
 from preprocessing import find_single_bit_ff, debanking_all
-from banking import cluster_and_merge_flip_flops, create_pin_mapping, resolve_overlaps, banking_each_clock_net
+from banking import cluster_and_merge_flip_flops, create_pin_mapping, resolve_overlaps, banking_each_clock_net, find_closest_row, calculate_placement, find_adjacent_empty_site
 from output import generate_output_file
 import statistics
 import re
@@ -16,13 +16,17 @@ def run_checker(input_file,output_file): # Checker must be in same directory as 
     result = subprocess.run(["./main",input_file,output_file],capture_output = True);
     success = str(result.stdout); # returns text if checker was successful
     
+    #print("success:",success);
+
     success = success.replace("\\n","\n");
     split_success = success.splitlines();
     
     decreased_slack = {}; # Key = String with new register name, Values = old pins & old and new slacks
 
     for line in split_success:
+        #print("line: ",line);
         if "timing change on pin" in line:
+            #print("timing change occured");
             split_line = line.split();
             old_pin = split_line[4];
             old_slack = split_line[5];
@@ -30,19 +34,21 @@ def run_checker(input_file,output_file): # Checker must be in same directory as 
             new_slack = split_line[7];
 
             if float(new_slack) < float(old_slack) and float(new_slack) < 0:# Checks if slack got more negative (worse)
-                print("slack got more negative for a pin in the new design");
+                #print("slack got more negative for a pin in the new design");
                 decreased_slack[new_pin] = {new_slack,old_pin,old_slack};
-                print("Pin in new design with bad slack: ",new_pin);
-                print("Pin in old design whose slack got changed: ",old_pin);
+                #print("Pin in new design with bad slack: ",new_pin);
+                #print("Pin in old design whose slack got changed: ",old_pin);
 
     return decreased_slack; # Return a dictionary of bad slack pins and associated register names in current design
 
 
-def debanking_some(die, cell_lib, netlist, decreased_slack): # decreased_slack is a dictionary
+def debanking_some(die, cell_lib, netlist, decreased_slack,parsed_data): # decreased_slack is a dictionary
     
     new_instances = {}
     ff_count = 0
     single_bit_ff_type = find_single_bit_ff(cell_lib)
+
+    debanked_instances = {}
 
     for inst_name, inst in list(die.instances.items()):
         if inst.cell_type in cell_lib.flip_flops:        
@@ -60,13 +66,15 @@ def debanking_some(die, cell_lib, netlist, decreased_slack): # decreased_slack i
                 already_mapped_pins = [];
 
                 for bit in range(ff.bits):
-                    new_name = f"{inst.name}_bit{bit}"; 
-                    new_inst = Instance(new_name, single_bit_ff_type, inst.x, inst.y + bit * 5)
+                    new_name = f"{inst.name}_bit{bit}";
+                    new_inst = Instance(new_name, single_bit_ff_type, inst.x, inst.y) # Make y the same so that it can trigger overlap that can be fixed.
                     new_inst.original_x = inst.original_x # Don't need to store intermediate mapping info 
                     new_inst.original_y = inst.original_y # The debanked FFs new info should be old instance info
                     new_inst.original_cell_type = inst.original_cell_type  
                     new_inst.original_name = inst.original_name 
                     new_instances[new_name] = new_inst
+
+                    debanked_instances[new_name] = new_inst;
                     
                     d_mapped = False;
                     q_mapped = False;
@@ -85,8 +93,8 @@ def debanking_some(die, cell_lib, netlist, decreased_slack): # decreased_slack i
                             if d_mapped == True and q_mapped == True:
                                 break
 
-                    print("banked FF instance's pin_mapping: ", inst.pin_mapping);
-                    print("new instance's pin_mapping: ", new_inst.pin_mapping);                
+                    #print("banked FF instance's pin_mapping: ", inst.pin_mapping);
+                    #print("new instance's pin_mapping: ", new_inst.pin_mapping);                
                 del die.instances[inst_name]
 
                 # update netlist
@@ -121,6 +129,19 @@ def debanking_some(die, cell_lib, netlist, decreased_slack): # decreased_slack i
 
     die.instances = new_instances
 
+    # Place debanked instances into legal bottom-left coords if not already in legal location.
+    placement_rows = parsed_data.placement_rows
+    #print(placement_rows);
+
+    #for inst in debanked_instances:
+        #at_legal_y = False;
+        #for row in placement_rows:
+            #if inst.y == row[1]:
+                #at_legal_y = True
+        #if not at_legal_y:
+            #closest_row = find_closest_row(inst.y,placement_rows);
+        
+
 
 if __name__ == "__main__":
     
@@ -140,7 +161,7 @@ if __name__ == "__main__":
     resolve_overlaps(parsed_data);
 
     # Generate output file of current design 
-    generate_output_file(parsed_data);
+    #generate_output_file(parsed_data);
 
     # Run checker using test input and generated output file.
     input_file = str("bm/sampleCase");
@@ -153,12 +174,12 @@ if __name__ == "__main__":
     die = parsed_data.die;
     cell_lib = parsed_data.cell_library;
     netlist = parsed_data.netlist;
-    #debanking_some(die,cell_lib,netlist,decreased_slack); 
+    debanking_some(die,cell_lib,netlist,decreased_slack,parsed_data); 
 
     # FINAL Placement fixing HERE
 
     # Run generate_output_file here for final design.
-    generate_output_file(parsed_data);
+    #generate_output_file(parsed_data);
     # Make sure names of new instances do not match any of the old names. Easy fix: just add an increasing value to the end of each name string
 
 
